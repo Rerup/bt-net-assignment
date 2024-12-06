f17999fadb89d76f9bfd9658b81ddd4019b9b75f
Rerup <jacob@techdrobe.com>
2024-12-06 14:55:45 +0100

    Add Product entity

---
+++ Domain/Products/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Products;
+
+public class Product
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    [MinLength(1), MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [Range(0.01, 99999)]
+    public decimal Price { get; set; }
+
+    [Column(TypeName = "text")]
+    public string Description { get; set; } = string.Empty;
+}

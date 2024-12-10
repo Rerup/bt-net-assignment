83cb73c4e823a6dc402aadcd566fe44743854565
Rerup <jacob@techdrobe.com>
2024-12-10 12:43:26 +0100

    Add InMemory Caching on GET routes

--- ProductGateway/Program.cs
+++ ProductGateway/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
